aa47cad2ee3fd252ff72298f1c40a6e7bdd64f6a
TonySeek <tonyseek@gmail.com>
2013-12-07 01:54:56 +0800

    compile the tclip source with setuptools.

---
+++ setup.py
@@ -0,0 +1,33 @@
+from subprocess import check_output
+from setuptools import setup, find_packages, Extension
+
+
+def pkgconfig(package, **kwargs):
+    flag_map = {"-I": "include_dirs", "-L": "library_dirs", "-l": "libraries"}
+    output = check_output(["pkg-config", "--cflags", "--libs", package])
+    for token in output.split():
+        if token[:2] in flag_map:
+            kwargs.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+        else:
+            kwargs.setdefault("extra_link_args", []).append(token)
+    return kwargs
+
+
+tclip_ext = Extension(
+    name="_tclip",
+    sources=["tclip/_tclip.cpp"],
+    language="c++",
+    **pkgconfig("opencv")
+)
+
+
+setup(
+    name="python-tclip",
+    author="Jiangge Zhang",
+    author_email="tonyseek@gmail.com",
+    version="0.1.0",
+    zip_safe=False,
+    url="https://github.com/tonyseek/python-tclip",
+    packages=find_packages(),
+    ext_modules=[tclip_ext],
+)
